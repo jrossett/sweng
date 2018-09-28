693866027ee0f41330418a371047c7619413a38d
jrossett <jeremie.rossetti@epfl.ch>
2018-09-28 15:42:50 +0200

    GreetingActivity done and test pass

--- Bootcamp/app/src/main/java/ch/epfl/sweng/bootcamp/MainActivity.java
+++ Bootcamp/app/src/main/java/ch/epfl/sweng/bootcamp/MainActivity.java
@@ -1,7 +1,10 @@
 package ch.epfl.sweng.bootcamp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, GreetingActivity.class);
+        EditText editText = findViewById(R.id.mainName);
+        intent.putExtra("name", editText.getText().toString());
+        startActivity(intent);
+
+    }
+
 }
