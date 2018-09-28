eaba1d04eaf8d7f5b1a377f02c0a89eae07c4331
jrossett <jeremie.rossetti@epfl.ch>
2018-09-28 11:04:10 +0200

    Compile without error

--- Bootcamp/app/src/androidTest/java/ch/epfl/sweng/bootcamp/MainActivityTest.java
+++ Bootcamp/app/src/androidTest/java/ch/epfl/sweng/bootcamp/MainActivityTest.java
@@ -1,17 +1,19 @@
 package ch.epfl.sweng.bootcamp;
+
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import static android.support.test.espresso.Espresso.onView;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.junit.Assert.*;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
